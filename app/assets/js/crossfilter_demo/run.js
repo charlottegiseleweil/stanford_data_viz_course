25f1985e504a1c4251d0d8b2c6b01c5f2dc1f914
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2016-10-18 13:31:19 -0700

    initial commit

--- app/assets/js/crossfilter_demo/run.js
+++ app/assets/js/crossfilter_demo/run.js
@@ -1,5 +1,6 @@
 var hist = function(data_in, chart_id, value, chart_title) {
 
+        
     var margin = {
             "top": 30,
             "right": 30,
@@ -92,166 +93,14 @@ var hist = function(data_in, chart_id, value, chart_title) {
         .selectAll("text")
         .style("text-anchor", "end")
         .attr("font-size", 10);
+
+        
 }
 
 
-d3.json(fuels_dataset,
-    function(error, fuels_dataset) {
-
-        /* Crossfilter constructor*/
-        var cf = crossfilter(fuels_dataset);
-
-        /*Dimensions (=columns of interest*/
-        var dim_input1 = cf.dimension(function(d) {
-            return d.Cellulosic_cost;
-        });
-        var dim_input2 = cf.dimension(function(d) {
-            return d.Tot_Biomass;
-        });
-        var dim_input3 = cf.dimension(function(d) {
-            return d.Feedstock_dist;
-        });
-        var dim_input4 = cf.dimension(function(d) {
-            return d.Cellulosic_yield;
-        });
-        var dim_input5 = cf.dimension(function(d) {
-            return d.Oil_elasticity;
-        });
-        var dim_input6 = cf.dimension(function(d) {
-            return d.Demand_elasticity;
-        });
-        var dim_input7 = cf.dimension(function(d) {
-            return d.Electricity_coproduction;
-        });
-        var dim_input8 = cf.dimension(function(d) {
-            return d.oil_supply_shift;
-        });
-        var dim_input9 = cf.dimension(function(d) {
-            return d.Biomass_bckstop_price;
-        });
-
-        var dim_output = cf.dimension(function(d) {
-            return d.Expen_change;
-        });
-
-        var dim_quant1 = cf.dimension(function(d) {
-            return d.Quant1;
-        });
-        var dim_quant2 = cf.dimension(function(d) {
-            return d.Quant2;
-        });
-        var dim_quant3 = cf.dimension(function(d) {
-            return d.Quant3;
-        });
-        var dim_quant4 = cf.dimension(function(d) {
-            return d.Quant4;
-        });
-        var dim_quant5 = cf.dimension(function(d) {
-            return d.Quant5;
-        });
-        var dim_quant6 = cf.dimension(function(d) {
-            return d.Quant6;
-        });
-        var dim_quant7 = cf.dimension(function(d) {
-            return d.Quant7;
-        });
-        var dim_quant8 = cf.dimension(function(d) {
-            return d.Quant8;
-        });
-        var dim_quant9 = cf.dimension(function(d) {
-            return d.Quant9;
-        });
-        var dim_quant10 = cf.dimension(function(d) {
-            return d.Quant10;
-        });
-
-        console.log(dim_input4.top(2));
-        console.log(dim_input9.bottom(2));
-        console.log(dim_input8.top(Infinity));
-
-        /*Grouping */
-        var group_1 = dim_quant1.group();
-        var group_2 = dim_quant2.group();
-        var group_3 = dim_quant3.group();
-        var group_4 = dim_quant4.group();
-        var group_5 = dim_quant5.group();
-        var group_6 = dim_quant6.group();
-        var group_7 = dim_quant7.group();
-        var group_8 = dim_quant8.group();
-        var group_9 = dim_quant9.group();
-        var group_10 = dim_quant10.group();
 
 
-
-        /* 
-            // sanity check
-    
-            group_team
-              .top(Infinity)
-              .forEach(function(d, i) {
-                console.log(JSON.stringify(d));
-              });
-              
-            */
-
-        /* --------------------------------------------------------- 
-    
-              Add a third and 4th variable to this map reduction
-              - the third should be the minimum year
-              - the fourth should be the maximum year
-              - hint: use inequalities
-              
-            */
-
-        var reduce_init = function() {
-            return {
-                "count": 0,
-                "total": 0
-            };
-        }
-
-        var reduce_add = function(p, v, nf) {
-            ++p.count;
-            p.total += v.g_all;
-            return p;
-        }
-
-        var reduce_remove = function(p, v, nf) {
-            --p.count;
-            p.total -= v.g_all;
-            return p;
-        }
-
-        /* --------------------------------------------------------- NOT SURE I UNDERSTAND more complex reductions*/
-
-        group_1.reduce(reduce_add, reduce_remove, reduce_init);
-        group_2.reduce(reduce_add, reduce_remove, reduce_init);
-        group_3.reduce(reduce_add, reduce_remove, reduce_init);
-        group_4.reduce(reduce_add, reduce_remove, reduce_init);
-        group_5.reduce(reduce_add, reduce_remove, reduce_init);
-        group_6.reduce(reduce_add, reduce_remove, reduce_init);
-        group_7.reduce(reduce_add, reduce_remove, reduce_init);
-        group_8.reduce(reduce_add, reduce_remove, reduce_init);
-        group_9.reduce(reduce_add, reduce_remove, reduce_init);
-        group_10.reduce(reduce_add, reduce_remove, reduce_init);
-      
-
-        var render_plots = function() {
-            // count refers to a specific key specified in reduce_init 
-            // and updated in reduce_add and reduce_subtract
-            // Modify this for the chart to plot the specified variable on the y-axis
-            hist(group_1.top(Infinity),
-                "appearances_by_quantiles",
-                "count",
-                "# of datapoints in this cost range"
-            );
-
-            /* build more charts here */
-
-        }
-
-
-        /* --------------------------------------------------------- 
+/* --------------------------------------------------------- 
            Sliders
         */
         var input1_slider = new Slider(
@@ -327,6 +176,162 @@ d3.json(fuels_dataset,
                 "value": [90, 200]
             });
 
+        
+d3.json("http://0.0.0.0:5050/data/fuel/", function(error, fuels_dataset) {
+
+        /* Crossfilter constructor*/
+        var cf = crossfilter(fuels_dataset);
+
+        /*Dimensions (=columns of interest*/
+        var dim_input1 = cf.dimension(function(d) {
+            return d.Cellulosic_cost;
+        });
+        var dim_input2 = cf.dimension(function(d) {
+            return d.Tot_Biomass;
+        });
+        var dim_input3 = cf.dimension(function(d) {
+            return d.Feedstock_dist;
+        });
+        var dim_input4 = cf.dimension(function(d) {
+            return d.Cellulosic_yield;
+        });
+        var dim_input5 = cf.dimension(function(d) {
+            return d.Oil_elasticity;
+        });
+        var dim_input6 = cf.dimension(function(d) {
+            return d.Demand_elasticity;
+        });
+        var dim_input7 = cf.dimension(function(d) {
+            return d.Electricity_coproduction;
+        });
+        var dim_input8 = cf.dimension(function(d) {
+            return d.oil_supply_shift;
+        });
+        var dim_input9 = cf.dimension(function(d) {
+            return d.Biomass_bckstop_price;
+        });
+
+        var dim_output = cf.dimension(function(d) {
+            return d.Expen_change;
+        });
+
+        var dim_quant1 = cf.dimension(function(d) {
+            return d.Quant1;
+        });
+        var dim_quant2 = cf.dimension(function(d) {
+            return d.Quant2;
+        });
+        var dim_quant3 = cf.dimension(function(d) {
+            return d.Quant3;
+        });
+        var dim_quant4 = cf.dimension(function(d) {
+            return d.Quant4;
+        });
+        var dim_quant5 = cf.dimension(function(d) {
+            return d.Quant5;
+        });
+        var dim_quant6 = cf.dimension(function(d) {
+            return d.Quant6;
+        });
+        var dim_quant7 = cf.dimension(function(d) {
+            return d.Quant7;
+        });
+        var dim_quant8 = cf.dimension(function(d) {
+            return d.Quant8;
+        });
+        var dim_quant9 = cf.dimension(function(d) {
+            return d.Quant9;
+        });
+        var dim_quant10 = cf.dimension(function(d) {
+            return d.Quant10;
+        });
+
+    
+        /*Grouping */
+        var group_1 = dim_quant1.group();
+        var group_2 = dim_quant2.group();
+        var group_3 = dim_quant3.group();
+        var group_4 = dim_quant4.group();
+        var group_5 = dim_quant5.group();
+        var group_6 = dim_quant6.group();
+        var group_7 = dim_quant7.group();
+        var group_8 = dim_quant8.group();
+        var group_9 = dim_quant9.group();
+        var group_10 = dim_quant10.group();
+
+
+
+        /* 
+            // sanity check
+    
+            group_team
+              .top(Infinity)
+              .forEach(function(d, i) {
+                console.log(JSON.stringify(d));
+              });
+              
+            */
+
+        /* --------------------------------------------------------- 
+    
+              Add a third and 4th variable to this map reduction
+              - the third should be the minimum year
+              - the fourth should be the maximum year
+              - hint: use inequalities
+              
+            */
+
+        var reduce_init = function() {
+            return {
+                "count": 0,
+                "total": 0
+            };
+        }
+
+        var reduce_add = function(p, v, nf) {
+            ++p.count;
+            p.total += v.g_all;
+            return p;
+        }
+
+        var reduce_remove = function(p, v, nf) {
+            --p.count;
+            p.total -= v.g_all;
+            return p;
+        }
+
+        /* --------------------------------------------------------- NOT SURE I UNDERSTAND more complex reductions*/
+
+        group_1.reduce(reduce_add, reduce_remove, reduce_init);
+        group_2.reduce(reduce_add, reduce_remove, reduce_init);
+        group_3.reduce(reduce_add, reduce_remove, reduce_init);
+        group_4.reduce(reduce_add, reduce_remove, reduce_init);
+        group_5.reduce(reduce_add, reduce_remove, reduce_init);
+        group_6.reduce(reduce_add, reduce_remove, reduce_init);
+        group_7.reduce(reduce_add, reduce_remove, reduce_init);
+        group_8.reduce(reduce_add, reduce_remove, reduce_init);
+        group_9.reduce(reduce_add, reduce_remove, reduce_init);
+        group_10.reduce(reduce_add, reduce_remove, reduce_init);
+      
+
+        var render_plots = function() {
+            // count refers to a specific key specified in reduce_init 
+            // and updated in reduce_add and reduce_subtract
+            // Modify this for the chart to plot the specified variable on the y-axis
+            hist(group_1.top(Infinity), group_2.top(Infinity),
+        group_3.top(Infinity)
+                "appearances_by_quantiles",
+                "count",
+                "Number of datapoints per cost range"
+            );
+
+            /* build more charts here */
+
+        }
+
+
+        
+
 
         // Event handlers for the sliders (?????) +  // filter based on the UI element ////////not sure I understand 
 
@@ -335,81 +340,63 @@ d3.json(fuels_dataset,
             dim_input1.filter(e);
             // re-render
             render_plots();
-            /* update the other charts here 
-             hint: each one of your event handlers needs to update all of the charts
-            */
+           
         });
         input2_slider.on("slide", function(e2) {
             d3.select("#input2_slider_txt").text("min: " + e2[0] + ", max: " + e2[1]);
             dim_input2.filter(e2);
             // re-render
             render_plots();
-            /* update the other charts here 
-             hint: each one of your event handlers needs to update all of the charts
-            */
+            
         });
         input3_slider.on("slide", function(e3) {
             d3.select("#input3_slider_txt").text("min: " + e3[0] + ", max: " + e3[1]);
             dim_input3.filter(e3);
             // re-render
             render_plots();
-            /* update the other charts here 
-             hint: each one of your event handlers needs to update all of the charts
-            */
+            
         });
         input4_slider.on("slide", function(e4) {
             d3.select("#input4_slider_txt").text("min: " + e4[0] + ", max: " + e4[1]);
             dim_input4.filter(e4);
             // re-render
             render_plots();
-            /* update the other charts here 
-             hint: each one of your event handlers needs to update all of the charts
-            */
+            
         });
         input5_slider.on("slide", function(e5) {
             d3.select("#input5_slider_txt").text("min: " + e5[0] + ", max: " + e5[1]);
             dim_input5.filter(e5);
             // re-render
             render_plots();
-            /* update the other charts here 
-             hint: each one of your event handlers needs to update all of the charts
-            */
+            
         });
         input6_slider.on("slide", function(e6) {
             d3.select("#input6_slider_txt").text("min: " + e6[0] + ", max: " + e6[1]);
             dim_input6.filter(e6);
             // re-render
             render_plots();
-            /* update the other charts here 
-             hint: each one of your event handlers needs to update all of the charts
-            */
+            
         });
         input7_slider.on("slide", function(e7) {
             d3.select("#input7_slider_txt").text("min: " + e7[0] + ", max: " + e7[1]);
             dim_input7.filter(e7);
             // re-render
             render_plots();
-            /* update the other charts here 
-             hint: each one of your event handlers needs to update all of the charts
-            */
+            
         });
         input8_slider.on("slide", function(e8) {
             d3.select("#input8_slider_txt").text("min: " + e8[0] + ", max: " + e8[1]);
             dim_input8.filter(e8);
             // re-render
             render_plots();
-            /* update the other charts here 
-             hint: each one of your event handlers needs to update all of the charts
-            */
+            
         });
         input9_slider.on("slide", function(e9) {
             d3.select("#input9_slider_txt").text("min: " + e9[0] + ", max: " + e9[1]);
             dim_input9.filter(e9);
             // re-render
             render_plots();
-            /* update the other charts here 
-             hint: each one of your event handlers needs to update all of the charts
-            */
+            
         });
 
 
@@ -426,5 +413,4 @@ d3.json(fuels_dataset,
 
         render_plots(); // this just renders the plots for the first time ????????????????????? why a second time ????
 
-        console.log("Everything worked");
     });
